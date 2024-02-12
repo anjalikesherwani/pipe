5cfb2644db6f1f5fb55803ec8f49d9dfa4ec8e87
anjalikesherwani <122610979+anjalikesherwani@users.noreply.github.com>
2024-02-12 12:34:59 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pipe';
+  currentDate: Date=new Date();
+  x:string = "My name is ANJALI";
+  y:string = "My name is ANJALI";
+  // z=0.567;
+  z=0.34;
+  a:{[key:string]:any}={
+    b:1,
+    c:"Hello World",
+    d:123
+  }
+
+  students:any={
+    ID:1, 
+    Name:"Anjali",
+    Age:25,
+    City:"Allahabad" ,
+    Country:"India",
+    Gender:"female",
+    DOB: new Date(),
+    Fees:200
+  };
+
 }
+
